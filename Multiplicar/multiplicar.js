a599737756fe9ff2ff6a97f0ede9282343fc253f
Miriam Guzman <mgg_pjl@yahoo.com>
2019-01-04 14:30:58 -0600

    Using yargs, creating a txt from terminal

--- Multiplicar/multiplicar.js
+++ Multiplicar/multiplicar.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
 
-crearArchivo = (base) => {
+let listarTabla = (base, limite = 10) => {
+
+    for( let i = 1; i <= limite; i++){
+            
+        console.log(`${base} * ${i} = ${base *i}`);
+
+    }
+}
+
+
+let crearArchivo = (base, limite = 10) => {
 
     return new Promise ((resolve, reject) => {
 
@@ -11,35 +21,26 @@ crearArchivo = (base) => {
 
         let data = '';
 
-        for( let i = 1; i <= 10; i++){
+        for( let i = 1; i <= limite; i++){
             
             data += (`${base} * ${i} = ${base *i}\n`);
 
         }
 
-        fs.writeFile(`Tablas/tabla-${base}.txt`, data, (err) => {
+        fs.writeFile(`Tablas/tabla-${base}-al-${limite}.txt`, data, (err) => {
 
             if(err) reject(err)
 
             else
 
-                resolve(`tabla ${base}.txt`)
+                resolve(`tabla ${base}-al-${limite}.txt`)
 
-                console.log('el archivo tabla-2.txt ha sido creado');
         });
 
     })
 }
 
 module.exports = {
-    crearArchivo
-}
-
-
-
-
-
-
-
-
-
+    crearArchivo, 
+    listarTabla
+};
--- app.js
+++ app.js
@@ -1,10 +1,52 @@
-const {crearArchivo} = require('./Multiplicar/multiplicar')
+const argv = require('yargs')
+    .command('listar', 'Imprime en la consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b', 
+        },
+            limite: {
+                alias: 'l',
+                default: 10
+        }
+    
+    })
+    .command('crear', 'Genera un archivo con la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b', 
+        },
+            limite: {
+                alias: 'l',
+                default: 10
+        }
+    
+    })
+    .help()
+    .argv;
 
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1]
 
-crearArchivo(base)
-    .then ( archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch(err => console.log(err))
+const {crearArchivo, listarTabla} = require('./Multiplicar/multiplicar')
+
+
+let command = argv._[0]; /* ._ hace referencia al objeto*/
+
+switch( command ){
+
+    case 'listar':
+        listarTabla( argv.base, argv.limite );
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then ( archivo => console.log(`Archivo creado: ${archivo}`))
+            .catch(err => console.log(err))
+        break;
+
+    default:
+    console.log('Comando no reconocido');
+}
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1]
+
